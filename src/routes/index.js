7c6316c039baa4d838ee93c1366ceac3972150ba
Athos <athospugliesedev@gmail.com>
2023-11-25 14:54:21 -0300

    auth login, expo fonts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,45 +1,60 @@
 import React from "react";
+import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
 import Settings from "../screens/Settings";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { CustomDrawer } from "./drawer";
+import { CustomDrawer, DrawerIcon } from "./drawer";
 import SignIn from "../screens/SignIn";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-export default function Routes() {
+const DrawerScreens = () => (
+  <Drawer.Navigator
+    defaultStatus="closed"
+    screenOptions={{
+      overlayColor: 'transparent',
+      drawerType: 'slide',
+      headerTintColor: '#111',
+    }}
+    drawerContent={CustomDrawer}
+  >
+    <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Screen name="Settings" component={Settings} />
+  </Drawer.Navigator>
+);
+
+const Routes = () => {
+  const headerLeft = React.useCallback(
+    (props) => {
+      return <DrawerIcon {...props} />;
+    },
+    [],
+  );
+
   return (
     <NavigationContainer
-    initialRouteName="SignIn"
     >
-      <Drawer.Navigator
-        defaultStatus="closed"
+      <Stack.Navigator
+        initialRouteName="Login"
         screenOptions={{
-          overlayColor: 'transparent',
-          drawerType: 'slide',
-          headerTintColor: '#111',
+          headerShown: false,
         }}
-        drawerContent={CustomDrawer}
       >
-        <Drawer.Screen name="Home">
-          {() => (
-            <Stack.Navigator
-              screenOptions={{
-                headerShown: false,
-              }}
-            >
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name="Login" component={Login} />
-              <Stack.Screen name="SignIn" component={SignIn} />
-            </Stack.Navigator>
-          )}
-        </Drawer.Screen>
-        <Drawer.Screen name="Settings" component={Settings} />
-      </Drawer.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen
+          name="DrawerScreens"
+          component={DrawerScreens}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+
     </NavigationContainer>
   );
-}
+};
+
+export default Routes;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,28 +1,55 @@
+import React, { useState } from 'react'
 import { Container, Description, Title } from "./styles"
 import { Button, Input, Label, Switch, XStack, YStack } from 'tamagui'
 import { Image } from 'react-native'
 import pombabranca from '../../assets/PombaBranca_PNG.png'
+import { colors } from '../../utils/colors'
+import axios from 'axios'
+import { useUser } from '../../contexts/userContext'
 
-export default function Login(){
-  return(
-   <YStack fullscreen bg={"$red10"} justifyContent="center" alignItems="center" padding="$2" gap="$2">
-    <Title>
-      Tela de Login
-    </Title>
-    <Image source={pombabranca} height={250} width={100}/>
-    <Input id="email" placeholder="aluno.00000@unicap.br"  />
-    <Input  id="password" paddingHorizontal={72} placeholder="Senha" textContentType="password" />
-    <Button  size="$3" theme="active" >
-          <Button.Text>
-            Entrar
-          </Button.Text>
-        </Button>
-        <Button size="$3" variant="outlined">
-          <Button.Text>
-            Criar conta
-          </Button.Text>
-        </Button>
-    <Description></Description>
-   </YStack>
+export default function Login({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setLoggedInUserId } = useUser();
+
+
+  const authLogin = async () => {
+    try {
+      const response = await axios.post('http://server-gold-pi.vercel.app/users/authenticate', {
+        email,
+        password,
+      });
+
+      const userId = response.data.id;
+      setLoggedInUserId(userId);
+
+      navigation.navigate('DrawerScreens', { userId });
+    } catch (error) {
+      console.error('Erro de autenticação:', error);
+    }
+  };
+
+
+
+  return (
+    <YStack fullscreen bg={"$red10"} justifyContent="center" alignItems="center" padding="$2" gap="$2" minWidth={250} space="$2">
+      <Title>
+        Tela de Login
+      </Title>
+      <Image source={pombabranca} style={{ width: 115, height: 100 }} resizeMode='stretch' />
+      <Input id="email" placeholder="aluno.00000@unicap.br" value={email} onChangeText={setEmail} height={40} width={250} borderWidth={2} borderColor={colors.coolGray[200]} />
+      <Input id="password" placeholder="Senha" textContentType="password" secureTextEntry value={password} onChangeText={setPassword} height={40} width={250} borderWidth={2} borderColor={colors.coolGray[200]} />
+      <Button size="$3" theme="active" onPress={authLogin}>
+        <Button.Text>
+          Entrar
+        </Button.Text>
+      </Button>
+      <Button size="$3" variant="outlined">
+        <Button.Text>
+          Criar conta
+        </Button.Text>
+      </Button>
+      <Description></Description>
+    </YStack>
   )
 }
